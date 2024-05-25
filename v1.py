293e6ab157db95c53e54ee3289d2d91a164ecfd7
Kole Leung <148280251+GameFriday@users.noreply.github.com>
2024-05-25 02:32:42 -0700

    Create v1.py

---
+++ v1.py
@@ -0,0 +1,3 @@
+sandwich_type = input("Please choose the type of sandwich (Chicken $5.25, Beef $6.25, or Tofu $5.75): ")
+print("You choose:", sandwich_type)
+#output of would be "You choose "sandwich_type""
